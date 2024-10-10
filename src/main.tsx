ceb7f8d506ed98a9c0c8def63af30bc55d2ae5af
Luis Crespo <lcrespom@gmail.com>
2024-10-10 19:17:04 +0200

    Message passing

--- src/main.tsx
+++ src/main.tsx
@@ -2,8 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 
+import { listen } from "@tauri-apps/api/event";
+
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
+  </React.StrictMode>
 );
+
+listen<string>("tash-command", (event) => {
+  console.log("Got event:", event);
+});
+console.log("Listening to tash-command events");
