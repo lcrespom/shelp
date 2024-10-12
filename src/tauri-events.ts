91c86be55df1653a96e0825d14d5e94e7100a074
Luis Crespo <lcrespom@gmail.com>
2024-10-12 22:30:09 +0200

    Working on webview commands

--- src/tauri-events.ts
+++ src/tauri-events.ts
@@ -4,10 +4,25 @@ import { isTauri } from '@tauri-apps/api/core'
 export function listenTauriEvents() {
   if (isTauri()) {
     listen<string>('tash-command', event => {
-      console.log('Got event:', event)
+      console.log('Got command:', event)
+      runCommand(event.payload)
     })
     console.log('Listening to tash-command events')
   } else {
     console.log('Tauri not detected (maybe running directly from browser?)')
   }
+}
+
+function runCommand(cmd: string) {
+  let [verb, query] = cmd.substring(1).split('?')
+  let params = new URLSearchParams(query || '')
+  if (!commands[verb]) console.warn('Unrecognized command')
+  else commands[verb](params)
+}
+
+const commands: Record<string, Function> = {
+  // Called when the user changes directory
+  chpwd(params: URLSearchParams) {
+    console.log(`ToDo: add ${params.get('dir')} to dirHistory`)
+  },
 }
