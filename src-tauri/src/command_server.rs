0119c1ed12c788389ec98a2e81006098055ef4eb
Luis Crespo <lcrespom@gmail.com>
2024-10-12 19:29:59 +0200

    Handling FE and BE commands

--- src-tauri/src/command_server.rs
+++ src-tauri/src/command_server.rs
@@ -12,13 +12,28 @@ pub fn setup_http_server(app: &mut tauri::App) {
         let server = Server::http(SERVER_ADDRESS).unwrap();
         println!("Server listening in {}", SERVER_ADDRESS);
         for request in server.incoming_requests() {
-            println!("Got request: {}", request.url());
-            //let message = format!("Message from HTTP request: {}", request.url().);
+            let url = request.url();
+            println!("Got request: {}", url);
+            let response = if url.starts_with("/_") {
+                handle_back_end_command(url)
+            } else {
+                handle_front_end_command(url)
+            };
             // Send the event to the Tauri webview
-            main_window.emit("tash-command", request.url()).unwrap();
+            main_window.emit("tash-command", url).unwrap();
             // Respond to the HTTP request
-            let response = Response::from_string("Event sent to Tauri webview");
+            let response = Response::from_string(response);
             request.respond(response).unwrap();
         }
     });
+}
+
+fn handle_back_end_command(_url: &str) -> &str {
+    println!("Back end command");
+    "Command handled by Tauri back end\n"
+}
+
+fn handle_front_end_command(_url: &str) -> &str {
+    println!("Front end command");
+    "Command sent to Tauri webview\n"
 }
