c6d74531607baa33051840db3988a6d1fd68b894
Luis Crespo <lcrespom@gmail.com>
2024-10-12 19:18:37 +0200

    Added Tauri detection

--- src/tauri-events.ts
+++ src/tauri-events.ts
@@ -1,8 +1,13 @@
 import { listen } from '@tauri-apps/api/event'
+import { isTauri } from '@tauri-apps/api/core'
 
 export function listenTauriEvents() {
-  listen<string>('tash-command', event => {
-    console.log('Got event:', event)
-  })
-  console.log('Listening to tash-command events')
+  if (isTauri()) {
+    listen<string>('tash-command', event => {
+      console.log('Got event:', event)
+    })
+    console.log('Listening to tash-command events')
+  } else {
+    console.log('Tauri not detected (maybe running directly from browser?)')
+  }
 }
