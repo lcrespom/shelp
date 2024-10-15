998dd8713c7d2ef0919dfe7eb5388b9f702083b2
Luis Crespo <lcrespom@gmail.com>
2024-10-15 21:45:55 +0200

    Removed nav, layout dirHistory

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,10 @@
-import { NavLink, Outlet } from 'react-router-dom'
+import { Outlet } from 'react-router-dom'
 
 import './App.css'
 
-function NavBar() {
-  return (
-    <>
-      <h2 className="text-2xl">Pages</h2>
-      <nav>
-        <ul>
-          <li>
-            <NavLink to="/">Welcome</NavLink>
-          </li>
-          <li>
-            <NavLink to="/demo">FE to BE demo</NavLink>
-          </li>
-          <li>
-            <NavLink to="/dirhistory">Dir History</NavLink>
-          </li>
-        </ul>
-      </nav>
-    </>
-  )
-}
-
 function App() {
   return (
-    <div className="m-2">
-      <h1 className="text-3xl">Shelp</h1>
-      <NavBar />
+    <div>
       <Outlet /> {/* Where child routes will render */}
     </div>
   )
--- src/pages/dirhistory.tsx
+++ src/pages/dirhistory.tsx
@@ -2,11 +2,12 @@ import { useState } from 'react'
 import { getDirHistory } from '../commands/dirhistory'
 
 export default function DirHistory() {
-  let [dirs, setDirs] = useState(getDirHistory())
+  let fullHistory = getDirHistory()
+  let [dirs, setDirs] = useState(fullHistory)
 
   function updateFilter(evt: React.FormEvent) {
     let filterText = (evt.target as HTMLInputElement).value || ''
-    let filteredDirs = getDirHistory().filter(dir =>
+    let filteredDirs = fullHistory.filter(dir =>
       dir.toLowerCase().includes(filterText.toLowerCase())
     )
     setDirs(filteredDirs)
@@ -14,22 +15,21 @@ export default function DirHistory() {
   }
 
   return (
-    <div>
-      <h2 className="text-2xl">ToDo: Dir History</h2>
-      <input
-        type="text"
-        className="mb-2 w-full rounded border border-gray-400 p-1 focus:outline-none"
-        autoFocus
-        spellCheck="false"
-        onInput={updateFilter}
-      />
-      <div className="flex flex-col rounded border border-gray-400 p-1">
+    <div className="flex h-screen flex-col">
+      <div className="m-2 flex flex-1 flex-col overflow-auto rounded border border-gray-400 p-1">
         {dirs.map((dir, idx) => (
           <a href="#" className="border-b font-mono text-sm hover:bg-blue-400" key={idx}>
             {dir}
           </a>
         ))}
       </div>
+      <input
+        type="text"
+        className="m-2 rounded border border-gray-400 p-1 font-mono focus:outline-none"
+        autoFocus
+        spellCheck="false"
+        onInput={updateFilter}
+      />
     </div>
   )
 }
