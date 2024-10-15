8856cf599100ef31c0bb95dc09b701481defb3ec
Luis Crespo <lcrespom@gmail.com>
2024-10-15 22:09:41 +0200

    Scroll dir list to bottom

--- src/pages/dirhistory.tsx
+++ src/pages/dirhistory.tsx
@@ -1,9 +1,16 @@
-import { useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import { getDirHistory } from '../commands/dirhistory'
 
 export default function DirHistory() {
   let fullHistory = getDirHistory()
   let [dirs, setDirs] = useState(fullHistory)
+  const scrollRef = useRef<HTMLDivElement | null>(null)
+
+  useEffect(() => {
+    if (scrollRef.current) {
+      scrollRef.current.scrollTop = scrollRef.current.scrollHeight
+    }
+  }, [dirs])
 
   function updateFilter(evt: React.FormEvent) {
     let filterText = (evt.target as HTMLInputElement).value || ''
@@ -16,13 +23,23 @@ export default function DirHistory() {
 
   return (
     <div className="flex h-screen flex-col">
-      <div className="m-2 flex flex-1 flex-col overflow-auto rounded border border-gray-400 p-1">
+      {/*------------------------- List of directories -------------------------*/}
+      <div
+        ref={scrollRef}
+        className="m-2 flex flex-1 flex-col overflow-auto rounded border border-gray-400 p-1"
+      >
         {dirs.map((dir, idx) => (
-          <a href="#" className="border-b font-mono text-sm hover:bg-blue-400" key={idx}>
+          <a
+            href="#"
+            className="border-b font-mono text-sm last:border-b-0 hover:bg-blue-400"
+            key={idx}
+          >
             {dir}
           </a>
         ))}
       </div>
+
+      {/*------------------------- Search box -------------------------*/}
       <input
         type="text"
         className="m-2 rounded border border-gray-400 p-1 font-mono focus:outline-none"
