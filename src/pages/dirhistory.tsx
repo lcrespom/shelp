34916b73d401a7eb587d44711f450d74cf009ad4
Luis Crespo <lcrespom@gmail.com>
2024-10-15 22:31:37 +0200

    Working on dark mode

--- src/pages/dirhistory.tsx
+++ src/pages/dirhistory.tsx
@@ -21,17 +21,21 @@ export default function DirHistory() {
     console.log(`Dir History: found ${filteredDirs.length} entries`)
   }
 
+  const dirListClasses = `
+    m-2 flex flex-1 flex-col overflow-auto rounded border border-gray-400 p-1
+    dark:bg-gray-800 dark:text-gray-200`
+  const inputClasses = `
+    m-2 rounded border border-gray-400 p-1 font-mono focus:outline-none
+    dark:bg-gray-800 dark:text-gray-200`
+
   return (
-    <div className="flex h-screen flex-col">
+    <div className="flex h-screen flex-col dark:bg-black">
       {/*------------------------- List of directories -------------------------*/}
-      <div
-        ref={scrollRef}
-        className="m-2 flex flex-1 flex-col overflow-auto rounded border border-gray-400 p-1"
-      >
+      <div ref={scrollRef} className={dirListClasses}>
         {dirs.map((dir, idx) => (
           <a
             href="#"
-            className="border-b font-mono text-sm last:border-b-0 hover:bg-blue-400"
+            className="border-b font-mono text-sm last:border-b-0 hover:bg-blue-500"
             key={idx}
           >
             {dir}
@@ -42,7 +46,7 @@ export default function DirHistory() {
       {/*------------------------- Search box -------------------------*/}
       <input
         type="text"
-        className="m-2 rounded border border-gray-400 p-1 font-mono focus:outline-none"
+        className={inputClasses}
         autoFocus
         spellCheck="false"
         onInput={updateFilter}
