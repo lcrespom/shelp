7027875d8b60a0ad5ed1d1456838f6478e99e13a
Luis Crespo <lcrespom@gmail.com>
2024-10-16 22:10:35 +0200

    Keyboard navigation

--- src/pages/dirhistory.tsx
+++ src/pages/dirhistory.tsx
@@ -1,9 +1,13 @@
 import { useEffect, useRef, useState } from 'react'
+import { invoke } from '@tauri-apps/api/core'
+import { getCurrentWindow } from '@tauri-apps/api/window'
+
 import { getDirHistory } from '../commands/dirhistory'
 
 export default function DirHistory() {
   let fullHistory = getDirHistory()
   let [dirs, setDirs] = useState(fullHistory)
+  let [row, setRow] = useState(dirs.length - 1)
   const scrollRef = useRef<HTMLDivElement | null>(null)
 
   useEffect(() => {
@@ -12,15 +16,50 @@ export default function DirHistory() {
     }
   }, [dirs])
 
+  // #region ------------------------- Event handlers -------------------------
+
   function updateFilter(evt: React.FormEvent) {
     let filterText = (evt.target as HTMLInputElement).value || ''
     let filteredDirs = fullHistory.filter(dir =>
       dir.toLowerCase().includes(filterText.toLowerCase())
     )
     setDirs(filteredDirs)
+    setRow(filteredDirs.length - 1)
     console.log(`Dir History: found ${filteredDirs.length} entries`)
   }
 
+  function checkKey(evt: React.KeyboardEvent) {
+    let key = evt.key || evt.code
+    let cancelEvent = true
+    if (key == 'Enter') {
+      invoke('send_response', { data: dirs[row] })
+      getCurrentWindow().hide()
+    } else if (key == 'ArrowUp') {
+      // TODO ensure row is visible, update scrollTop if required
+      if (row > 0) setRow(row - 1)
+    } else if (key == 'ArrowDown') {
+      // TODO ensure row is visible, update scrollTop if required
+      if (row < dirs.length - 1) setRow(row + 1)
+    } else if (key == 'PageUp') {
+      // TODO page up
+      // Requires computation of number of visible rows
+      console.log('ToDo: page up')
+    } else if (key == 'PageDown') {
+      // TODO page down
+      // Requires computation of number of visible rows
+      console.log('ToDo: page down')
+    } else {
+      cancelEvent = false
+    }
+    if (cancelEvent) evt.preventDefault()
+  }
+
+  function selectDirectory(dir: string) {
+    invoke('send_response', { data: dir })
+    getCurrentWindow().hide()
+  }
+
+  // #region ------------------------- Component rendering -------------------------
   return (
     <div className="flex h-screen flex-col dark:bg-black">
       {/*------------------------- List of directories -------------------------*/}
@@ -29,8 +68,9 @@ export default function DirHistory() {
           {dirs.map((dir, idx) => (
             <a
               href="#"
-              className="block border-t p-0.5 font-mono text-sm hover:bg-blue-300 dark:border-gray-600 dark:hover:bg-blue-600"
+              className={`${row == idx ? 'bg-blue-300 dark:bg-blue-600' : 'hover:bg-blue-100 dark:hover:bg-blue-800'} block border-t p-0.5 font-mono text-sm dark:border-gray-600`}
               key={idx}
+              onClick={_ => selectDirectory(dir)}
             >
               {dir}
             </a>
@@ -45,6 +85,7 @@ export default function DirHistory() {
         autoFocus
         spellCheck="false"
         onInput={updateFilter}
+        onKeyDown={checkKey}
       />
     </div>
   )
