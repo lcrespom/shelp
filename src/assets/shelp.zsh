13ac07f4bc3a97060548304b0838938fdcbe1db6
Luis Crespo <lcrespom@gmail.com>
2024-10-17 18:00:10 +0200

    Improved performance of popup

--- src/assets/shelp.zsh
+++ src/assets/shelp.zsh
@@ -1,13 +1,14 @@
+PAGE_DOWN="^[[6~"
+
 POPUP_HOST="localhost:5431"
 
-PAGE_DOWN="^[[6~"
+TERMAPP=$(osascript -e 'tell application "System Events" to get the name of the first application process whose frontmost is true')
 
 chpwd() {
     curl -s -o /dev/null "127.0.0.1:5431/chpwd?dir=$PWD"
 }
 
 function dir_history_popup() {
-    TERMAPP=$(osascript -e 'tell application "System Events" to get the name of the first application process whose frontmost is true')
     new_dir=$(curl -s "$POPUP_HOST/dirHistory")
     if [[ -n "$new_dir" ]]; then
         cd $new_dir
