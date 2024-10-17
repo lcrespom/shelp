b8f08e224281043e04772b6ee40b21fe772a1636
Luis Crespo <lcrespom@gmail.com>
2024-10-17 19:34:11 +0200

    Documented shelp.zsh

--- src/assets/shelp.zsh
+++ src/assets/shelp.zsh
@@ -1,13 +1,19 @@
+# Key codes
+PAGE_UP="^[[5~"
 PAGE_DOWN="^[[6~"
 
+# shelp command server address
 POPUP_HOST="localhost:5431"
 
+# Name of the terminal application
 TERMAPP=$(osascript -e 'tell application "System Events" to get the name of the first application process whose frontmost is true')
 
+# Record every time the user changes directory
 chpwd() {
     curl -s -o /dev/null "127.0.0.1:5431/chpwd?dir=$PWD"
 }
 
+# Open shelp popup in the dir history page
 function dir_history_popup() {
     new_dir=$(curl -s "$POPUP_HOST/dirHistory")
     if [[ -n "$new_dir" ]]; then
@@ -17,7 +23,7 @@ function dir_history_popup() {
     osascript -e "tell application \"$TERMAPP\" to activate"
 }
 
-# Register the function as a widget
+# Register the dirhistory function as a widget
 zle -N dir_history_popup
 
 # Bind the key to the widget
