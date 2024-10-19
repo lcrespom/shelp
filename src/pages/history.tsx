54c9db457fe044b8ad70e925ef540f17b575b9f4
Luis Crespo <lcrespom@gmail.com>
2024-10-19 09:27:17 +0200

    Removed history duplicates at start

--- src/pages/history.tsx
+++ src/pages/history.tsx
@@ -3,7 +3,12 @@ import SelectList from '../components/select-list'
 let history: string[] = []
 
 export function setHistory(hist: string[]) {
-  history = hist.filter((item, idx) => item && hist.indexOf(item) === idx)
+  // Remove empty lines and duplicates.
+  // The list is reversed so that duplicates at removed at the start of the list.
+  history = hist
+    .reverse()
+    .filter((item, idx) => item && hist.indexOf(item) === idx)
+    .reverse()
 }
 
 export default function History() {
