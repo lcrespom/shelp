35ec6a175b27a87b99511127a2589f959cc82d02
Luis Crespo <lcrespom@gmail.com>
2024-10-23 15:36:06 +0200

    Working on direntry component

--- src/components/dir-entry.tsx
+++ src/components/dir-entry.tsx
@@ -8,9 +8,20 @@ type DirEntryProps = {
 export default function DirEntry(props: DirEntryProps) {
   let info = getDirInfo(props.line)
   return (
-    <>
-      {info.permissions} | {info.user} {info.group} | {info.size} | {info.date},{' '}
-      {info.time.slice(0, -3)} | {info.name}
-    </>
+    <div className="flex">
+      <span className="hl-assignment w-28 overflow-x-clip pr-2">{info.permissions}</span>
+      <span className="hl-program w-14 overflow-x-clip pr-2 text-center font-sans">
+        {info.user}
+      </span>
+      <span className="hl-program w-14 overflow-x-clip pr-2 text-center font-sans">
+        {info.group}
+      </span>
+      <span className="hl-quote w-20 overflow-x-clip pr-4 text-right">{info.size}</span>
+      <span className="hl-option w-36 overflow-x-clip whitespace-pre pr-4 text-right font-sans">
+        {info.date} {info.time.slice(0, -3)}
+      </span>
+      {/* TODO: file hilight depending on permissions */}
+      <span className="overflow-x-clip overflow-ellipsis">{info.name}</span>
+    </div>
   )
 }
