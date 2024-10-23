ef2b05c11e633cf5587630435f7da913acef2d2d
Luis Crespo <lcrespom@gmail.com>
2024-10-23 08:40:42 +0200

    Updated file-search page to use rowComponent

--- src/pages/file-search.tsx
+++ src/pages/file-search.tsx
@@ -1,6 +1,9 @@
 import { getDirInfo } from '../commands/file-search'
+import MatchHighlight from '../components/match-highlight'
 import SelectList from '../components/select-list'
 
 export default function FileSearch() {
-  return <SelectList list={getDirInfo().map(di => di.name)} />
+  return (
+    <SelectList list={getDirInfo().map(di => di.name)} rowComponent={MatchHighlight} />
+  )
 }
