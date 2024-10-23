adca8bd24c4b91cdce45d72f9adb9bc9a4c9d82d
Luis Crespo <lcrespom@gmail.com>
2024-10-23 20:19:13 +0200

    CSS refactor

--- src/components/dir-entry.tsx
+++ src/components/dir-entry.tsx
@@ -14,26 +14,15 @@ function fileColor(permissions: string) {
 export default function DirEntry(props: DirEntryProps) {
   let info = getDirInfo(props.line)
   return (
-    <div className="flex">
-      <span className="hl-assignment w-26 overflow-x-clip pl-1 pr-2">
-        {info.permissions}
-      </span>
-      <span className="hl-program w-14 overflow-x-clip pr-2 text-center font-sans">
-        {info.user}
-      </span>
-      <span className="hl-program w-14 overflow-x-clip pr-2 text-center font-sans">
-        {info.group}
-      </span>
-      <span className="hl-quote w-20 overflow-x-clip pr-4 text-right">{info.size}</span>
-      <span className="hl-option w-36 overflow-x-clip whitespace-pre pr-4 text-right font-sans">
+    <div className="dir-entry flex">
+      <span className="dir-permissions">{info.permissions}</span>
+      <span className="dir-user">{info.user}</span>
+      <span className="dir-group">{info.group}</span>
+      <span className="dir-size">{info.size}</span>
+      <span className="dir-date">
         {info.date} {info.time.slice(0, -3)}
       </span>
-      {/* TODO: file hilight depending on permissions */}
-      <span
-        className={`overflow-x-clip overflow-ellipsis ${fileColor(info.permissions)}`}
-      >
-        {info.name}
-      </span>
+      <span className={`dir-file ${fileColor(info.permissions)}`}>{info.name}</span>
     </div>
   )
 }
